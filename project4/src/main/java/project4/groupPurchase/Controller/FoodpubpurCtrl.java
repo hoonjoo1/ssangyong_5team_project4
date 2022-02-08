3c6cf02093d0dc7c01ac4352808bac058a2b9414
HoonJoo Jang <hoonjoo1@naver.com>
2022-02-08 22:42:12 +0900

    2022-02-08 장훈주 컨트롤러 수정

--- project4/src/main/java/project4/groupPurchase/Controller/FoodpubpurCtrl.java
+++ project4/src/main/java/project4/groupPurchase/Controller/FoodpubpurCtrl.java
@@ -4,12 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class writeBoard {
+public class FoodpubpurCtrl {
+	
+	@RequestMapping("/boardlist.do")
+	public String groupboardlist() {
+		
+		return "WEB-INF\\views\\groupPurchase\\BoardList.jsp";
+	}
+	
+	@RequestMapping("/groupread.do")
+	public String groupread() {
+		
+		return "WEB-INF\\views\\groupPurchase\\readBoard.jsp";
+	}
 	
 	@RequestMapping("/groupwrite.do")
 	public String groupwrite() {
 		
 		return "WEB-INF\\views\\groupPurchase\\writeBoard.jsp";
 	}
-	
 }
